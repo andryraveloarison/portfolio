8969e0165428a06f22804d8eeadf2bec406ce023
raveloarison <andryraveloarison01@gmail.com>
2024-05-24 16:12:56 +0300

    base animations texte

--- src/components/Projects/ProjectInfo/ProjectInfo.jsx
+++ src/components/Projects/ProjectInfo/ProjectInfo.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./ProjectInfo.module.css";
 import projects from "../../../data/projects.json";
 import { ProjectCard } from "../ProjectCard/ProjectCard";
+import { useRef } from "react";
+import { useScroll, motion, useTransform } from "framer-motion";
+
 
 export const Projects = () => {
+  const title = "All porject real"
+  const titleRef = useRef(null)
+  const {scrollYProgress} = useScroll({
+    target: titleRef,
+    offset:['start end','start start']
+  })
+
+  const words = title.split(" ")
+  useEffect(()=>{
+
+    scrollYProgress.on('change',e => console.log(e))
+
+  },[])
+  
   return (
     <section className={styles.container} id="projects">
-      <h2 className={styles.title}>Projects</h2>
+      <h2 className={styles.title} ref={titleRef}>
+        {
+          words.map((word,i)=>{
+            const start = i/words.length
+            const end = i + (1/words.length)
+            return <Word key={i} range={[start, end]} progress ={scrollYProgress}>{word}</Word>
+          })
+        }
+        </h2>
       <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project} />;
@@ -15,3 +40,18 @@ export const Projects = () => {
     </section>
   );
 };
+
+
+
+const Word = ({children, range, progress})=>{
+  const opacity = useTransform(progress, range, [0,1])
+  return (
+      <span className={styles.word} >
+        <span className={styles.shadow}> {children}</span>
+        <motion.span style={{opacity}}>
+          {children}
+        </motion.span>
+      </span>
+
+  )
+}
