f045cfc902d62190691b4bca04c062e883f45f08
Andry <andryraveloarison01@gmail.com>
2025-03-18 15:31:13 +0300

    web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import { Analytics } from "@vercel/analytics/react"
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
