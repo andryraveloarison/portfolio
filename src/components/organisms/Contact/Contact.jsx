4c54e58e7082b68b550c61fbddc60777b0197fab
andry <andryraveloarison01@gmail.com>
2026-03-07 12:08:35 +0300

    footer

--- src/components/organisms/Contact/Contact.jsx
+++ src/components/organisms/Contact/Contact.jsx
@@ -11,22 +11,22 @@ export const Contact = () => {
 
   const title = "CONTACT ME "
   const titleRef = useRef(null)
-  const {scrollYProgress} = useScroll({
+  const { scrollYProgress } = useScroll({
     target: titleRef,
-    offset:['start end','start 0.25']
+    offset: ['start end', 'start 0.25']
   })
 
   const words = title.split(" ")
 
-  
+
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2 className={styles.title} ref={titleRef}> 
+        <h2 className={styles.title} ref={titleRef}>
           {
-            words.map((word,i)=>{
+            words.map((word, i) => {
               const start = i / words.length
-              return <Word key={i} range={[start, 1]} progress ={scrollYProgress}>{word}</Word>
+              return <Word key={i} range={[start, 1]} progress={scrollYProgress}>{word}</Word>
             })
           }
         </h2>
@@ -36,9 +36,9 @@ export const Contact = () => {
 
         {
           contacts.map((contact, id) => {
-            return(
+            return (
               <li key={id} className={styles.link}>
-                <img src={getImageUrl(contact.imageSrc)} alt="Email icon" className={styles.image}/>
+                <img src={getImageUrl(contact.imageSrc)} alt="Email icon" className={styles.image} />
                 <a href={contact.source} target="_blank">{contact.name}</a>
               </li>
             )
@@ -49,7 +49,7 @@ export const Contact = () => {
 
       <div className={styles.footer}>
         <p>Designed and build by Andry </p>
-        <p>©Copyright 2024 - Andry RAVELOARISON</p>
+        <p>© Copyright {new Date().getFullYear()} - Andry RAVELOARISON</p>
       </div>
     </footer>
   );
